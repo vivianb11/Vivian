00db33f82abad652e59e436cb454c29149ccea4f
vivianb11 <vivianb.info@gmail.com>
2023-02-21 23:56:44 -0500

    troll

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>information</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <section>
+
+      <form method="POST" action="reponce.php">
+  			<label for="nom">Nom :</label>
+  			<input type="text" name="nom" id="nom" autofocus="true">
+  			<label for="prenom">Prenom :</label>
+  			<input type="text" name="prenom" id="prenom">
+        <input type="submit" name="submit">
+      </form>
+
+    </section>
+  </body>
+</html>
