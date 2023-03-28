f1d918bbea3541c024ca180fd8f02d5df552b6a4
vivianb11 <vivianb.info@gmail.com>
2023-03-28 00:36:06 -0400

    Update V1.1.2
    Added mail copy and adjusted text alignment

--- script.js
+++ script.js
@@ -9,4 +9,44 @@ copyLink.addEventListener('click', () => {
         .catch(err => {
             console.error('Could not copy text: ', err);
         });
+
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.innerHTML = `
+    <div class="popup-content">
+      <h2>Email Copied!</h2>
+      <p>The email has been copied to the clipboard</p>
+      <button id="closeButton">Close</button>
+    </div>
+  `;
+
+    document.body.appendChild(popup);
+
+    // Fait apparaitre le popup de la top de la page
+    popup.style.top = "-100%";
+    popup.style.opacity = "0";
+    popup.style.transition = "all 0.3s ease-in-out";
+    setTimeout(() => {
+        popup.style.top = "0";
+        popup.style.opacity = "1";
+    }, 100);
+
+    const closeButton = popup.querySelector('#closeButton');
+    closeButton.addEventListener('click', () => {
+        // Fait disparaitre le popup
+        popup.style.top = "-100%";
+        popup.style.opacity = "0";
+        setTimeout(() => {
+            document.body.removeChild(popup);
+        }, 300);
+    });
+
+    setTimeout(() => {
+        // Fait disparaitre le popup après 3 secondes
+        popup.style.top = "-100%";
+        popup.style.opacity = "0";
+        setTimeout(() => {
+            document.body.removeChild(popup);
+        }, 300);
+    }, 3000);
 });
